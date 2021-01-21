b3ba7a6cf0aaed79b92fa6b810c9c910bf5c974b
KhallilB <khallil.bailey@presidioidentity.com>
2021-01-21 01:08:32 -0800

    [add] singup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,83 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-const User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    isArtist: {
-        type: Boolean,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    updatedAt: {
-        type: Date 
+const User = require('../models/User');
+
+// TODO: Test these two functions and see if they work,
+// TODO: Work on client integration
+
+/**
+ * Registers a new user
+ */
+const signup = async (req, res) => {
+    let user = await new User();
+
+    user.username = req.body.username;
+    user.email = req.body.email;
+    let hashedPassword = bcrypt.hash(req.body.password, 10, function (err, hash){
+        if (err) {
+            console.error(err)
+            return
+        }    
+    })
+    user.password = hashedPassword;
+
+    await user.save((savedUser, err) => {
+        if(err) return res.status(500).send({ error: err, message:  'There was a problem registering user' });
+
+        let token = jwt.sign(
+            { userId: savedUser._id },
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXP }
+        );
+
+        return res.status(200).send({ auth: true, token: token });
+    })
+}
+
+/**
+ * Logs a registered user in
+ */
+const login = async (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if(username.includes('@')) {
+        User.findOne({ email: username }, (foundUser, err) =>{
+            if (err) return res.status(500).send({ error: err, message: 'Server error unable to login, please try agian' });
+            if (!foundUser) return res.status(404).send({ error: err, message: 'User not registered, please sign up' });
+
+            let validPass = bcrypt.compareSync(password, foundUser.password);
+            if(!validPass) return res.status(401).send({ error: err, message: 'Wrong username and/or password', auth: false });
+
+            let token = jwt.sign(
+                { userId: foundUser._id },
+                process.env.JWT_SECRET,
+                { expiresIn: process.env.JWT_EXP }
+            );
+            
+            return res.status(200).send({ auth: true, token })
+        })
     }
-})
 
-module.exports = mongoose.model('User', User)
+    User.findOne({ username: username }, (foundUser, err) =>{
+        if (err) return res.status(500).send({ error: err, message: 'Server error unable to login, please try agian' });
+        if (!foundUser) return res.status(404).send({ error: err, message: 'User not registered, please sign up' });
+
+        let validPass = bcrypt.compareSync(password, foundUser.password);
+        if(!validPass) return res.status(401).send({ error: err, message: 'Wrong username and/or password', auth: false });
+
+        let token = jwt.sign(
+            { userId: foundUser._id },
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXP }
+        );
+        
+        return res.status(200).send({ auth: true, token })
+    })
+}
+
+export default {
+    signup,
+    login
+}
